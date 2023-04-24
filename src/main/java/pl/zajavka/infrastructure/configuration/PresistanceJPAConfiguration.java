6b79b5cf104df5aefb2cace99d7fd66c2de38eb8
Waldemar Goszka Surface Pro <goszka.waldemar@gmail.com>
2023-04-24 20:55:22 +0200

    first commit

---
+++ src/main/java/pl/zajavka/infrastructure/configuration/PresistanceJPAConfiguration.java
@@ -0,0 +1,76 @@
+package pl.zajavka.infrastructure.configuration;
+
+import jakarta.persistence.EntityManagerFactory;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import pl.zajavka.infrastructure.database.jparepositories.JpaRepositoriesMarker;
+import pl.zajavka.infrastructure.database.model.EntityMarker;
+
+
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@AllArgsConstructor
+@EnableTransactionManagement
+@PropertySource({"classpath:database.properties"})
+@EnableJpaRepositories(basePackageClasses = JpaRepositoriesMarker.class)
+public class PresistanceJPAConfiguration {
+    private final Environment environment;
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        final LocalContainerEntityManagerFactoryBean entityManagerFactoryBean
+                = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource());
+        entityManagerFactoryBean.setPackagesToScan(EntityMarker.class.getPackageName());
+        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManagerFactoryBean.setJpaProperties(jpaProperties());
+        return entityManagerFactoryBean;
+    }
+
+    final Properties jpaProperties() {
+        final Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
+        hibernateProperties.setProperty("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+        hibernateProperties.setProperty("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
+        hibernateProperties.setProperty("hibernate.format_sql", environment.getProperty("hibernate.format_sql"));
+        return hibernateProperties;
+    }
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getProperty("jdbc.driverClassName","org.postgresql.Driver"));
+        dataSource.setUrl(environment.getProperty("jdbc.url","jdbc:postgresql://localhost:5432/zajavka_store"));
+        dataSource.setUsername(environment.getProperty("jdbc.user","postgres"));
+        dataSource.setPassword(environment.getProperty("jdbc.pass","postgres"));
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(final EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+
+
+}
